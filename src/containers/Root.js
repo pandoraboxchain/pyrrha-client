46a7ab03e21a9f82b1e69f3395fce900b7708563
Dr. Maxim Orlovsky <dr.orlovsky@gmail.com>
2017-12-19 22:26:31 +0200

    Added Web3

--- src/containers/Root.js
+++ src/containers/Root.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'react-router-redux';
+import { Web3Provider } from 'react-web3';
 import App from './App';
 import DevTools, { ImportExportTool } from './DevTools';
 import routes from '../config/routes.js';
@@ -21,8 +22,9 @@ export default class Root extends Component {
       <Provider store={store}>
         <ConnectedRouter history={history}>
           <div>
-            <App>{routes}</App>
-
+            <Web3Provider>
+              <App>{routes}</App>
+            </Web3Provider>
             {isDevEnv &&
               <div>
                 <DevTools />
